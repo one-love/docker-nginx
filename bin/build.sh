36dec082bd7b47747e744f5c4df7280f47e3310f
Goran Mekić <meka@lugons.org>
2015-09-18 20:33:07 +0200

    Wrong build

--- bin/build.sh
+++ bin/build.sh
@@ -2,18 +2,8 @@
 
 set -e
 
-PACKAGES="git-core libpython2.7 postgresql-server-dev-all python python-dev wget python-pip"
-
 apt-get update -y
 apt-get upgrade -y
-apt-get install -y $PACKAGES
-
-git clone https://github.com/one-love/backend.git
-cd backend
-mv /app/__init__.py project/settings
-pip install -r requirements.txt
-mkdir /static
-python manage.py collectstatic --noinput
 
 # Get consul-template
 wget https://github.com/hashicorp/consul-template/releases/download/v0.9.0/consul-template_0.9.0_linux_amd64.tar.gz -O /tmp/consul-template.tar.gz
@@ -22,8 +12,5 @@ tar xfvp consul-template.tar.gz
 mv consul-template*/consul-template /bin
 
 echo "Cleanup"
-cd ..
-rm -rf /usr/local backend
-apt-get purge -y $PACKAGES
 apt-get autoremove -y --purge
 apt-get clean -y
