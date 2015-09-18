8bae5e6acae70a874724cbf006b54cf5486e9626
Goran Mekić <meka@lugons.org>
2015-09-18 20:37:16 +0200

    Download nginx

--- bin/build.sh
+++ bin/build.sh
@@ -4,13 +4,15 @@ set -e
 
 apt-get update -y
 apt-get upgrade -y
+apt-get install -y wget
 
 # Get consul-template
-wget https://github.com/hashicorp/consul-template/releases/download/v0.9.0/consul-template_0.9.0_linux_amd64.tar.gz -O /tmp/consul-template.tar.gz
+wget https://github.com/hashicorp/consul-template/releases/download/v0.10.0/consul-template_0.10.0_linux_amd64.tar.gz -O /tmp/consul-template.tar.gz
 cd /tmp
 tar xfvp consul-template.tar.gz
 mv consul-template*/consul-template /bin
 
 echo "Cleanup"
+apt-get purge -y wget
 apt-get autoremove -y --purge
 apt-get clean -y
